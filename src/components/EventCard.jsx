f607cb8b4acfc53cd4b52eec8f470e16b56a8617
James Junior Baah <jjbaah068@gmail.com>
2025-06-04 15:38:34 -0700

    fourth-commit

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,9 +1,9 @@
-import eventCardImage from "../assets/images/event-card.png";
+// import eventCardImage from "../assets/images/event-card.png";
 
-export default function EventCard() {
+export default function EventCard({event}) {
     return(
         <div className="bg-white rounded-xl shadow-lg overflow-hidden py-7 px-5 ">
-            <img src={eventCardImage} alt="Event Card" />
+            <img src={event.image} alt="Event Card" />
             <h1 className="font-semibold py-2.5">BestSeller Book Bootcamp -write, Market & Publish Your Book -Lucknow</h1>
             <h2 className="text-[#7848F4] font-semibold py-4">Saturday, March 19, 9.30PM</h2>
             <h3 className="text-[#7E7E7E]">ONLINE EVENT- Attend anywhere</h3>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,75 +2,104 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Search } from "lucide-react";
 import EventCard from "../components/EventCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 export default function Home() {
+    // Declare state variable in React
+    const [events, setEvents] = useState([]);
+
+    // Define a function to fetch events from API
+    const getEvents = () => {
+        axios.get('https://fakestoreapi.com/products')
+            .then(response => {
+                setEvents(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    // Run fetcher based on side-effects 
+    useEffect(getEvents, []);
+
+
+
+
     return (
         <>
-    <Navbar />
-    <section className="bg-[url(./assets/images/hero-bg.png)] h-[596px] bg-no-repeat bg-cover w-[95%] mx-auto relative bg-[#f8f8fa]">
-        <h1 className="text-big-heading text-white w-[30%] mx-auto text-center pt-32 font-bold">MADE FOR THOSE WHO DO</h1>
-         <form className="bg-navy-blue w-[90%] mx-auto py-6 px-10 flex flex-row justify-between rounded-md absolute -bottom-12 left-[5%]">
-        <div className="flex flex-col w-[30%]">
+            <Navbar />
+            <section className="bg-[url(./assets/images/hero-bg.png)] h-[596px] bg-no-repeat bg-cover w-[95%] mx-auto relative bg-[#f8f8fa]">
+                <h1 className="text-big-heading text-white w-[30%] mx-auto text-center pt-32 font-bold">MADE FOR THOSE WHO DO</h1>
+                <form className="bg-navy-blue w-[90%] mx-auto py-6 px-10 flex flex-row justify-between rounded-md absolute -bottom-12 left-[5%]">
+                    <div className="flex flex-col w-[30%]">
 
-            <label htmlFor="type" className="text-white">Looking for</label>
-            <select name="type" id="type" className="bg-white p-2 rounded-md">
-                <option selected disabled>Choose event type</option>
-                <option value="drama">Drama</option>
-                <option value="tech">Tech</option>
-                </select>
-        </div>
-        <div className="flex flex-col w-[30%]">
+                        <label htmlFor="type" className="text-white">Looking for</label>
+                        <select name="type" id="type" className="bg-white p-2 rounded-md">
+                            <option selected disabled>Choose event type</option>
+                            <option value="drama">Drama</option>
+                            <option value="tech">Tech</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col w-[30%]">
 
-            <label htmlFor="type" className="text-white">Location</label>
-            <select name="location" id="location" className="bg-white p-2 rounded-md">
-                <option selected disabled>Choose location</option>
-                <option value="accra">Accra</option>
-                <option value="kumasi">Kumasi</option>
-                </select>
-        </div>
-        <div className="flex flex-col w-[30%]">
+                        <label htmlFor="type" className="text-white">Location</label>
+                        <select name="location" id="location" className="bg-white p-2 rounded-md">
+                            <option selected disabled>Choose location</option>
+                            <option value="accra">Accra</option>
+                            <option value="kumasi">Kumasi</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col w-[30%]">
 
-            <label htmlFor="type" className="text-white">When</label>
-            <input type="datetime-local" name="when" id="when" className="bg-white rounded-md p-1.5" />
-        </div>
-        <div className="bg-primary text-white px-7 flex flex-row justify-center items-center rounded-md">
-            <Search />
-        </div>
-    </form>
-    </section>
-    <section className="bg-[#f8f8fa]">
-        <div className="mt-[105px] ">
-            <h1 className="ml-24">
-                <span className="font-bold text-2xl">Upcoming</span>
-                <span className="font-bold text-2xl text-[#7848F4]"> Events</span>
-            </h1>
-            <div className="flex flex-row justify-end mr-10">
-                <select name="when" id="when">
-                    <option selected disabled>Weekdays</option>
-                    <option value="Monday">Monday</option>
-                    <option value="Tuesday">Tuesday</option>
-                </select>
-                <select name="event-type" id="event-type">
-                    <option selected disabled>Event Type</option>
-                    <option value="Festival">Festival</option>
-                    <option value="Concert">Concert</option>
-                </select>
-                <select name="any-category" id="any-category">
-                    <option selected disabled>Any Category</option>
-                    <option value="Arts Culture">Arts Culture</option>
-                    <option value="Community">Community</option>
-                </select>
-                
-            </div>
-        </div>
-        <div className="grid grid-cols-3 gap-5 p-12.5 ml-10 w-[90%]">
-            {[1,2,3,4,5,6].map(n => <EventCard key={n} />)}
-        </div>
-    </section>
-   
-   <section className="h-[500px]"></section>
-    <Footer />
-    </>
+                        <label htmlFor="type" className="text-white">When</label>
+                        <input type="datetime-local" name="when" id="when" className="bg-white rounded-md p-1.5" />
+                    </div>
+                    <div className="bg-primary text-white px-7 flex flex-row justify-center items-center rounded-md">
+                        <Search />
+                    </div>
+                </form>
+            </section>
+            <section className="bg-[#f8f8fa]">
+                <div className="mt-[105px] ">
+                    <h1 className="ml-24">
+                        <span className="font-bold text-2xl">Upcoming</span>
+                        <span className="font-bold text-2xl text-[#7848F4]"> Events</span>
+                    </h1>
+                    <div className="flex flex-row justify-end mr-15">
+                        <select name="when" id="when">
+                            <option selected disabled>Weekdays</option>
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                        </select>
+                        <select name="event-type" id="event-type">
+                            <option selected disabled>Event Type</option>
+                            <option value="Festival">Festival</option>
+                            <option value="Concert">Concert</option>
+                        </select>
+                        <select name="any-category" id="any-category">
+                            <option selected disabled>Any Category</option>
+                            <option value="Arts Culture">Arts Culture</option>
+                            <option value="Community">Community</option>
+                        </select>
+
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 gap-5 p-12.5 ml-10 w-[90%]">
+                    {events.map(item => {
+                        return (
+                            <EventCard 
+                            key={item.id} 
+                            event={item} />
+                        );
+                    })}
+                </div>
+            </section>
+
+            <section className="h-[500px]"></section>
+            <Footer />
+        </>
 
     );
 }
